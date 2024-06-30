919b03709077ecfdedfa47939041ffe9b4010533
Abdul Hafiz Ramadan <abdulhafizramadan.dev@gmail.com>
2024-06-30 22:04:22 +0700

    feat: finish the application

--- UITableViewStoryBoard/ViewController.swift
+++ UITableViewStoryBoard/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
+    private let names = [
+        [
+            "Abdul",
+            "Hafiz",
+            "Ramadan"
+        ],
+        [
+            "Abdul",
+            "Hafiz",
+            "Ramadan"
+        ]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+}
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row select at \(indexPath.section): \(indexPath.row)")
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = names[indexPath.section][indexPath.row]
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names[section].count
+    }
 }
 
